499d9ab8fdc6ea05ea9efa7d3fa3474f59207858
William Martins <wwwhmartins@gmail.com>
2016-05-11 01:13:49 -0300

    WIP to test integration between NEAD and spelunkbots

--- DLL Solution/Bot/SpelunkBot/PlaygroundBot.cpp
+++ DLL Solution/Bot/SpelunkBot/PlaygroundBot.cpp
@@ -1,8 +1,14 @@
 #include "stdafx.h"
 #include "PlaygroundBot.h"
 
+#include "../vendor/NEAT/innovation.h"
+
 void PlaygroundBot::Update()
 {
     std::cout << "I am a dummy bot" << std::endl;
     _goLeft = true;
+
+    NEAT::Innovation inn = NEAT::Innovation(1, 2, 3, 4, 5, false);
+
+    printf("%d\n", inn.node_in_id);
 }
